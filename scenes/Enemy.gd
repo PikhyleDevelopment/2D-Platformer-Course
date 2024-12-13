c74bd9e7d860f5f615186bab64e22532f970d5e4
Erik Mason <erik@pikhyle.net>
2024-12-12 23:12:37 -0500

    Creates Enemy death animation and logic. Tweaks EnemySpawner timer. Removes spike object used for testing in Level_001.
    Signed-off-by: Erik Mason <erik@pikhyle.net>

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 ## Global Variables
+var enemyDeathScene : Resource = preload("res://scenes/EnemyDeath.tscn")
 var maxSpeed : int = 25
 var velocity : Vector2 = Vector2.ZERO
 var direction : Vector2 = Vector2.ZERO 
@@ -12,10 +13,6 @@ func _ready():
 	direction = startDirection
 	$GoalDetector.connect("area_entered", self, "on_goal_entered")
 	$HitboxArea.connect("area_entered", self, "on_hitbox_entered")
-	
-func on_hitbox_entered(_area2d):
-	$"/root/Helpers".apply_camera_shake(1)
-	queue_free()
 
 func _process(delta):
 	velocity.x = (direction * maxSpeed).x
@@ -24,6 +21,18 @@ func _process(delta):
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	$AnimatedSprite.flip_h = true if direction.x > 0 else false
+
+func kill():
+	var deathInstance : Node = enemyDeathScene.instance()
+	get_parent().add_child(deathInstance)
+	deathInstance.global_position = global_position
+	if (velocity.x > 0):
+		deathInstance.scale = Vector2(-1, 1)
+	queue_free()
 	
 func on_goal_entered(_area2d):
 	direction *= -1
+
+func on_hitbox_entered(_area2d):
+	$"/root/Helpers".apply_camera_shake(1)
+	call_deferred("kill")
